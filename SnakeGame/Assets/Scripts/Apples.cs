1e35ef9f5be79ebf26d8c6a0af512b12bab72fe9
JuliKovalivker <101721164+JuliKovalivker@users.noreply.github.com>
2024-10-08 16:30:12 -0300

    feat: isntanias de manzanas y extencion de snake

--- SnakeGame/Assets/Scripts/Apples.cs
+++ SnakeGame/Assets/Scripts/Apples.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Apples : MonoBehaviour
 {
     public GameObject apple;
+    public GameObject snake2;
+    int counter = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +23,14 @@ public class Apples : MonoBehaviour
 
     void OnCollisionEnter(Collision col)
     {
-        Destroy(apple);
+        //Destroy(apple);
+        Instantiate(snake2);
+        snake2.transform.position -= new Vector3(1f, 0f, 0f);
+        counter++;
+        int rand1 = Random.Range(-8, 8); 
+        int rand2 = Random.Range(-3, 5); 
+        //Instantiate(apple);
+        apple.transform.position = new Vector3(rand1, rand2, -4f);
+
     }
 }
