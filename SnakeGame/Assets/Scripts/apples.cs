6f5b00a9af6423bc812d77cab70b9f725c48bc4d
JuliKovalivker <101721164+JuliKovalivker@users.noreply.github.com>
2024-11-14 20:41:57 -0300

    clase

--- SnakeGame/Assets/Scripts/apples.cs
+++ SnakeGame/Assets/Scripts/apples.cs
@@ -8,8 +8,8 @@ public class apples : MonoBehaviour
 
     void OnCollisionEnter(Collision col){
         //Destroy(apple);
-        int x_pos = Random.Range(-11, 11);
-        int y_pos = Random.Range(-6, 7);
-        apple.transform.position = new Vector3(x_pos, y_pos, 3.5f);
+        int x_pos = Random.Range(8, 8);
+        int y_pos = Random.Range(-3, 5);
+        apple.transform.position = new Vector3(x_pos, y_pos, -4f);
     }
 }
--- SnakeGame/Assets/Scripts/player.cs
+++ SnakeGame/Assets/Scripts/player.cs
@@ -5,38 +5,48 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public GameObject snake;
-    float x;
-    float y;
+    public GameObject segment;
+    //float x;
+    //float y;
+
+    private List<GameObject> snake_segments = new List<GameObject>();
+
+    private Vector3 input_direction = Vector3.right; //new Vector3(1f, 0f, 0f);
+    private Vector3 direction = Vector3.right;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        snake_segments.Add(snake);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) && direction != Vector3.left)
         {
-            x = 0.01f;
-            y = 0f;
+            input_direction = Vector3.right;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) && direction != Vector3.right)
         {
-            x = -0.01f;
-            y = 0f;
+            input_direction = Vector3.left;
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) && direction != Vector3.down)
         {
-            x = 0f;
-            y = 0.01f;
+            input_direction = Vector3.up;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) && direction != Vector3.up)
         {
-            x = 0f;
-            y = -0.01f;
+            input_direction = Vector3.down;
         }
-        snake.transform.position += new Vector3(x, y, 0f);
+        //snake.transform.position += new Vector3(x, y, 0f);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        GameObject new_segment = Instantiate(segment);
+        ////////////
+        new_segment.transform.position = snake.transform.position;
+        snake_segments.Add(new_segment);
     }
 }
